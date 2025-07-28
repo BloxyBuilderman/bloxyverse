64904a69b669494c89b296f5c2ff6520bad34726
BloxyBuilderman <rudranshgirigamer489502@gmail.com>
2025-07-28 17:39:34 +0530

    Update loading.js

--- loading.js
+++ loading.js
@@ -3,3 +3,11 @@ setTimeout(() => {
   console.log("Redirecting...");
   window.location.href = "index.html";
 }, 5000);
+
+function loadChapter(chapter) {
+  fetch(`chapters/chapter${chapter}.html`)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById('chapter-content').innerHTML = html;
+    });
+}
